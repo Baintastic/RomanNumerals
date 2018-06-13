0667141b9f4e68afe113e4a23f477d612a7f3000
Thabani Tembe <thabanitembe@hotmail.com>
2018-06-13 16:27:32 +0200

    began refactoring

---
+++ RomanNumerals/RomanNumerals.Tests/RomanNumeralsTests.cs
@@ -0,0 +1,82 @@
+﻿using NUnit.Framework;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RomanNumerals.Tests
+{
+    [TestFixture]
+    public class RomanNumeralsTests
+    {
+
+        [TestCase("I",1)]
+        [TestCase("III", 3)]
+        public void Convert_GivenANumberLessThan4_ShouldReturnItsRomanNumeral(string expected , int number)
+        {
+            //---------------Arrange-------------------
+            var sut = new RomanNumerals();
+            //---------------Act ----------------------
+            var actual = sut.Convert(number);
+            //---------------Assert -----------------------
+            Assert.AreEqual(expected, actual);
+        }
+
+        [TestCase("IV", 4)]
+        public void Convert_Given4_ShouldReturnItsRomanNumeral(string expected, int number)
+        {
+            //---------------Arrange-------------------
+            var sut = new RomanNumerals();
+            //---------------Act ----------------------
+            var actual = sut.Convert(number);
+            //---------------Assert -----------------------
+            Assert.AreEqual(expected, actual);
+        }
+
+
+        [TestCase("V", 5)]
+        [TestCase("VIII", 8)]
+        public void Convert_GivenANumberMoreThan4ButLessThan9_ShouldReturnItsRomanNumeral(string expected, int number)
+        {
+            //---------------Arrange-------------------
+            var sut = new RomanNumerals();
+            //---------------Act ----------------------
+            var actual = sut.Convert(number);
+            //---------------Assert -----------------------
+            Assert.AreEqual(expected, actual);
+        }
+
+
+        [TestCase("IX", 9)]
+        //[TestCase("VIII", 8)]
+        public void Convert_Given9_ShouldReturnItsRomanNumeral(string expected, int number)
+        {
+            //---------------Arrange-------------------
+            var sut = new RomanNumerals();
+            //---------------Act ----------------------
+            var actual = sut.Convert(number);
+            //---------------Assert -----------------------
+            Assert.AreEqual(expected, actual);
+        }
+
+        [TestCase("X", 10)]
+        [TestCase("XI", 11)]
+        [TestCase("XIX", 19)]
+        [TestCase("XXI", 21)]
+        [TestCase("XXV", 25)]
+        [TestCase("XXXIX", 39)]
+        public void Convert_GivenANumberMoreThan9ButLessThan40_ShouldReturnItsRomanNumeral(string expected, int number)
+        {
+            //---------------Arrange-------------------
+            var sut = new RomanNumerals();
+            //---------------Act ----------------------
+            var actual = sut.Convert(number);
+            //---------------Assert -----------------------
+            Assert.AreEqual(expected, actual);
+        }
+
+
+
+    }
+}
---
+++ RomanNumerals/RomanNumerals/RomanNumerals.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RomanNumerals
+{
+    public class RomanNumerals
+    {
+        public object Convert(int number)
+        {
+            string romanNumerals = "";
+            while (number != 0) {
+                if (number >= 10)
+                {
+                    romanNumerals += "X";
+                    number -= 10;
+                }
+                if (number <= 3)
+                {
+                    romanNumerals = AddRomanOnes(number, romanNumerals);
+                    number -= number;
+                }
+
+                if (number == 4)
+                {
+                    romanNumerals += "IV";
+                    number -= number;
+                }
+                if (number >= 5 && number <= 8)
+                {
+                    romanNumerals += "V";
+                    romanNumerals = AddRomanOnes(number - 5, romanNumerals);
+                    number -= number;
+                }
+                if (number == 9)
+                {
+                    romanNumerals += "IX";
+                    number -= number;
+                }
+
+            }
+
+            return romanNumerals;
+        }
+
+        private static string AddRomanOnes(int number, string romanNumeral)
+        {
+            for (int i = 0; i < number; i++)
+            {
+                romanNumeral += "I";
+            }
+
+            return romanNumeral;
+        }
+    }
+}
